0862a68a1bded79d9cce7b9b9c880230f9021c7a
Janabil <zhanabil.sydykov@bcc.kz>
2024-04-23 10:36:41 +0500

    create homepage.dart & pages

---
+++ lib/homapage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'pages/page_1.dart';
+import 'pages/page_2.dart';
+import 'pages/page_3.dart';
+import 'pages/page_4.dart';
+
+class HomePage extends StatelessWidget {
+  final PageController _controller = PageController();
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: 500,
+              child: PageView(
+                controller: _controller,
+                children: const [
+                  Page1(),
+                  Page2(),
+                  Page3(),
+                  Page4(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
